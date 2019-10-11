aea055422a0865e8ab1bbcf65bd36dd8f26ef870
isshun226 <51151631+isshun226@users.noreply.github.com>
2019-10-11 18:09:01 +0900

    Add css

--- todoList/src/Styles.js
+++ todoList/src/Styles.js
@@ -5,11 +5,30 @@ export default Styles = {
         height: '100%'
     },
     header: {
+        alignItems: 'center',
+        backgroundColor: '#eee',
+        borderBottomWidth: 1,
+        borderBottomColor: '#bbb',
         height: (getStatusBarHeight() * 3),
         justifyContent: 'center',
-        alignItems: 'center'
     },
     headerText: {
         fontSize: 36,
+    },
+    main: {
+        flex: 1
+    },
+    todoForm: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#99F84B',
+        fontSize: 24,
+        height: 24,
+        marginTop: 10,
+        outline: 0,
+        textAlign: 'center',
+        width: '80%',
+    },
+    todoList: {
+        flex: 1
     }
 }
